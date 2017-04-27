1605bbb8892e9f623e19f57d69d4d531dfd6363d
Rachel8595 <915589618@qq.com>
2017-04-27 11:33:15 +0800

    Update LoginClass.java

--- LoginClass.java
+++ LoginClass.java
@@ -39,6 +39,8 @@ public class LoginClass
          }  
          dr = new RemoteWebDriver(urlInstance,test);  
          dr.get(webSite);  
+	     
+	     System.out.println("keke")
      }  
 	   
 	/*@Test  
